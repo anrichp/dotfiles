2029703ae9b925755f6c03473f444a4b38304c99
Anrich Potgieter <anrichp@gmail.com>
2025-05-09 15:39:29 +0100

    add keymap for push

--- nvim/.config/nvim/lua/anrichp/plugins/vim_fugitive.lua
+++ nvim/.config/nvim/lua/anrichp/plugins/vim_fugitive.lua
@@ -1,6 +1,7 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [P]ush" })
+		vim.keymap.set("n", "<leader>gp", vim.cmd.Git("push"), { desc = "[G]it [P]ush" })
 	end,
 }
