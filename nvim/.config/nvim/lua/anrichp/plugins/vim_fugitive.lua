8e213d96d67e9669e00c3e4d1c4e75d7e4c510f0
Anrich Potgieter <anrichp@gmail.com>
2025-05-09 15:41:17 +0100

    fix issue with keybind call

--- nvim/.config/nvim/lua/anrichp/plugins/vim_fugitive.lua
+++ nvim/.config/nvim/lua/anrichp/plugins/vim_fugitive.lua
@@ -2,6 +2,8 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [P]ush" })
-		vim.keymap.set("n", "<leader>gp", vim.cmd.Git("push"), { desc = "[G]it [P]ush" })
+		vim.keymap.set("n", "<leader>gp", function()
+			vim.cmd.Git("push")
+		end, { desc = "[G]it [P]ush" })
 	end,
 }
