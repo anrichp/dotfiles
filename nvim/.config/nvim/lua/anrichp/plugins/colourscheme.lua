5a009b1e36bc2f68c69d2ca813dc41684cb7579a
Anrich Potgieter <anrichp@gmail.com>
2025-05-17 19:17:49 +0100

    add new plugin and general clean up

--- nvim/.config/nvim/lua/anrichp/plugins/colourscheme.lua
+++ nvim/.config/nvim/lua/anrichp/plugins/colourscheme.lua
@@ -1,10 +1,6 @@
-return { -- You can easily change to a different colorscheme.
-	-- Change the name of the colorscheme plugin below, and then
-	-- change the command in the config to whatever the name of that colorscheme is.
-	--
-	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+return {
 	"catppuccin/nvim",
-	priority = 1000, -- Make sure to load this before all the other start plugins.
+	priority = 1000,
 	config = function()
 		---@diagnostic disable-next-line: missing-fields
 		require("catppuccin").setup({
@@ -23,9 +19,6 @@ return { -- You can easily change to a different colorscheme.
 			},
 		})
 
-		-- Load the colorscheme here.
-		-- Like many other themes, this one has different styles, and you could load
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
