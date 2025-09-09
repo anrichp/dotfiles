2e3bd9b048b5d336062c95066388cb94fc8ce442
Anrich Potgieter <anrichp@gmail.com>
2025-09-09 09:03:25 +0100

    rebuilt nvim config with new pack man

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,63 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.signcolumn = "yes"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
+vim.pack.add({
+	{ src = "http://github.com/vague2k/vague.nvim" },
+	{ src = "http://github.com/stevearc/oil.nvim" },
+	{ src = "http://github.com/echasnovski/mini.pick" },
+	{ src = "http://github.com/neovim/nvim-lspconfig" },
+	{ src = "http://github.com/chomosuke/typst-preview.nvim" },
+})
+
+require "mini.pick".setup()
+require "oil".setup()
+require "typst-preview".setup()
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+vim.lsp.enable({ "lua_ls", "tinymist", "phpactor", "racket_langserver", "rust-analyzer" })
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			}
+		}
+	}
+})
+vim.lsp.config("rust-analyzer", {
+	filetypes = { "rust" },
+	cmd = { "/home/anrichp/.cargo/bin/rust-analyzer" },
+	settings = {
+		['rust-analyzer'] = {
+			diagnostics = {
+				enable = false,
+			}
+		}
+	}
+})
+vim.cmd("colorscheme vague")
+vim.cmd(":hi statusline guibg=NONE")
